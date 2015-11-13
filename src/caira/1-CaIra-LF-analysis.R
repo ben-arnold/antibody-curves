13234b6aace883281df54af9f2eeedabf4264093
ben-arnold <benarnold@berkeley.edu>
2015-11-13 14:50:11 -0800

    Updated Ca Ira analysis output files

--- src/caira/1-CaIra-LF-analysis.R
+++ src/caira/1-CaIra-LF-analysis.R
@@ -25,8 +25,6 @@
 rm(list=ls())
 library(SuperLearner)
 library(tmle)
-library(RColorBrewer)
-library(scales)
 
 
 # source the base functions for
@@ -60,8 +58,8 @@ d <- subset(d,age>=1)
 # add a half year to remove bias from rounded age measurement
 d$age <- d$age+0.5
 
-# note: round 2 was partial round (not much data), so for differences
-# compute round 3 versus round 1
+# note: round 2 was partial round (incomplete measurement), so for differences
+# compute round 3 versus round 1 (dropping round 2)
 d <- subset(d,round!=2)
 d$mda <- ifelse(d$round==3,1,0)
 
@@ -71,7 +69,6 @@ d$mda <- ifelse(d$round==3,1,0)
 # Age-specific antibody response
 # curves to the Wb123, Bm14, and
 # Bm33 antigens for each round 
-# (note: round 2 was partial round)
 #------------------------------------
 set.seed(1742)
 wb123.fit1 <- SLAb.curve(Y=log10(d$wb123[d$round==1]),Age=d$age[d$round==1],id=d$id[d$round==1])
@@ -93,7 +90,11 @@ bm33.fit3 <- SLAb.curve(Y=log10(d$bm33[d$round==3]),Age=d$age[d$round==3],id=d$i
 # overlap: 4.5 - 11.5 years
 #-------------------------------
 
+# subset the data to areas of age overlap
 ad <- subset(d,age>=4.5 & age<=11.5)
+# summarize the number of observations in
+# each measurement round
+table(ad$mda)
 
 set.seed(24734)
 mu.wb123 <- sapply(c(1,3),function(x) SLAb.tmle(
@@ -130,7 +131,7 @@ mu.bm33 <- sapply(c(1,3),function(x) SLAb.tmle(
   Y=log10(ad$bm33[ad$round==x]),
   Age=ad$age[ad$round==x],
   id=ad$id[ad$round==x]
-)
+  )
 )
 
 diff.bm33 <- SLAb.tmle(
